4653d789b8fff78481e48f65e41a03ade7fe5894
Slava <slava@ubuntu-slava.(none)>
2013-12-31 16:53:21 +0200

    Adds ssh config to workstation

--- nodes/chef-workstation/cookbooks/chef-workstation/recipes/configure-chef-workstation.rb
+++ nodes/chef-workstation/cookbooks/chef-workstation/recipes/configure-chef-workstation.rb
@@ -5,12 +5,41 @@ directory "/home/vagrant/.chef" do
     action :create
 end
 
-package "sshpass"
-
-execute "get admin pem" do
-	command "sshpass -p vagrant scp vagrant@192.168.33.10:/etc/chef-server/admin.pem ~/.chef"
+directory "/home/vagrant/.ssh" do
+    owner "vagrant" 
+    group "vagrant"
+    mode 0700 
+    action :create
 end
 
-execute "get validation pem" do
-	command "sshpass -p vagrant scp vagrant@192.168.33.10:/etc/chef-server/chef-validator.pem ~/.chef"
+cookbook_file "/home/vagrant/.ssh/id_rsa" do
+    mode 0600
+    owner "vagrant" 
+    group "vagrant"
+    source "id_rsa"
+    action :create
 end
+
+cookbook_file "/home/vagrant/.ssh/id_rsa.pub" do
+    mode 0644
+    owner "vagrant" 
+    group "vagrant"
+    source "id_rsa.pub"
+    action :create
+end
+
+cookbook_file "/home/vagrant/.ssh/known_hosts" do
+    mode 0644
+    owner "vagrant" 
+    group "vagrant"
+    source "known_hosts"
+    action :create
+end
+
+#execute "get admin pem" do
+#	command "sshpass -p vagrant scp vagrant@192.168.33.10:/etc/chef-server/admin.pem ~/.chef"
+#end
+
+#execute "get validation pem" do
+#	command "sshpass -p vagrant scp vagrant@192.168.33.10:/etc/chef-server/chef-validator.pem ~/.chef"
+#end
