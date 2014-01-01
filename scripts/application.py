6bd78967ee74f93bfd8f26819e2f4f3ede68d9dc
Slava <slava_f11@mail.ru>
2014-01-01 23:59:02 +0200

    Adds application node.

---
+++ scripts/application.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+from node_handler import NodeHandler
+import sys
+
+handler = NodeHandler("application", "192.168.33.13")
+for command in sys.argv:
+    if command == "up":
+        handler.run_server()
+    elif command == "restore":
+        handler.restore_server()
+    elif command == "halt":
+        handler.halt_server()
+    elif command == "destroy":
+        handler.destroy_server()
+    elif command == "ssh":
+        handler.ssh_server()
