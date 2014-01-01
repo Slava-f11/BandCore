a28ae1b577c29d1f3afb007d6d472da42695cc7b
Slava <slava_f11@mail.ru>
2014-01-01 23:48:58 +0200

    Removes chef-repo

--- chef-repo/.chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'slava'
-client_key               '/home/slava/Projects/BandBook/chef-repo/.chef/slava.pem'
-validation_client_name   'chef-validator'
-validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://slava-dell-system-vostro-3750:443'
-syntax_check_cache_path  '/home/slava/Projects/BandBook/chef-repo/.chef/syntax_check_cache'
